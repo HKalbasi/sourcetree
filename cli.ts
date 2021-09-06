c1d157b8640b71035fdb65cb952a2ad96c3e589d
hamidreza kalbasi <hamidrezakalbasi@protonmail.com>
2021-09-06 13:46:17 +0430

    fix dogfooding script

--- cli.ts
+++ cli.ts
@@ -7,9 +7,12 @@ program
   .option('-o, --output <file>', 'output folder for generated files', 'out');
 
 const cli = async () => {
+  const time = (new Date()).valueOf();
   program.parse(process.argv);
   const options: CliOptions = program.opts();
   main(options);
+  const duration = (new Date()).valueOf() - time;
+  console.log(`Finished building sourcetree of your code in ${duration / 1000} second`);
 };
 
 cli();
